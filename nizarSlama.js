a594a48934cd46f9d0ca6fd34c905da6c29e0679
Nizar64 <ns8@hotmail.fr>
2019-03-11 15:31:22 +0100

    add information profil

---
+++ nizarSlama.js
@@ -0,0 +1,15 @@
+let myAge = 24;
+const myName = "nizar Slama";
+let isInwcs = false;
+const schoolName = "Wild Code School";
+
+console.log(typeof myName, typeof myAge,);
+console.log(myAge,myName,isInwcs,schoolName);
+
+myAge++;
+
+isInwcs = 1;
+
+isInwcs = true;
+
+console.log (myAge,myName,isInwcs,schoolName)
