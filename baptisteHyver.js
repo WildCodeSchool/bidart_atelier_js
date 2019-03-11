7cad23c97bed99ceb68af95b8da2c37afaa162c1
Baptiste Hyver <baptiste.hyver@gmail.com>
2019-03-11 15:36:27 +0100

    Exercice variables in javascript

---
+++ baptisteHyver.js
@@ -0,0 +1,48 @@
+// -  un entier représentant ton âge
+
+let myAge = 26;
+
+// -  une chaîne de caractères, représentant ton nom et ton prénom
+
+const myName = "Baptiste Hyver";
+
+// -  un booléen représentant si tu es en formation à la Wild Code School, initialisée à false
+
+let isInWcs = false;
+
+// -  une chaîne de caractères représentant le nom de l’école
+
+const schoolName = "Wild Code School";
+
+// -  Afficher le type de chaque variable (typeof)
+
+typeof myAge;
+typeof myName;
+typeof isInWcs;
+typeof schoolName;
+
+console.log(typeof myAge, typeof myName, typeof isInWcs, typeof schoolName);
+
+// -  Afficher la valeur de chaque variable
+
+console.log(myAge, myName, isInWcs, schoolName);
+
+//    Continuer le programme avec les instructions suivantes:
+// -  modifier l’âge en ajoutant 1
+
+myAge++;
+
+// -  modifier le booléen représentant si tu es en formation à la Wild Code School pour qu’elle soit true
+
+isInWcs = true;
+
+// -  Afficher à nouveau les valeurs des variables modifiées
+
+console.log(myAge, isInWcs);
+
+
+
+
+
+
+
