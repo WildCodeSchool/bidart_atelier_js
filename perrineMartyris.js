6d723516f41fde02f01bbe168ebbd16342e42b65
Perrine Martyris <perrinemartyris@gmail.com>
2019-03-11 15:32:31 +0100

    perrine test js

---
+++ perrineMartyris.js
@@ -0,0 +1,8 @@
+let age = 36;
+const name = 'Martyris Perrine';
+let wild = false;
+const schoolName = 'Wild Code School';
+console.log(typeof age, typeof name, typeof wild, typeof schoolName);
+age++;
+wild = true;
+console.log (age, name, wild, schoolName);
