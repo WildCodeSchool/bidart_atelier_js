261dcf7191728b2fd26c5f34a4d0d1f254324570
Stephane Jacot <stephanejacot@icloud.com>
2019-03-11 15:24:01 +0100

    add stephanejacot.js

---
+++ stephanejacot.js
@@ -0,0 +1,12 @@
+let myAge = 46;
+const myName = "Stéphane Jacot";
+let isInWCS = false;
+const schoolName = "Wide Code School";
+
+console.log (typeof myAge, typeof myName, typeof isInWCS, typeof schoolName);
+console.log (myAge, myName, isInWCS, schoolName);
+
+myAge +=1
+isInWCS=true;
+console.log (myAge, myName, isInWCS, schoolName);
+
