b500f83049eb96bc631e4592d5750f77f9c8c302
BB5014 <bblabriz@bbox.fr>
2019-03-11 15:32:22 +0100

    exo ok

---
+++ nathalie.js
@@ -0,0 +1,12 @@
+let myAge = 52;
+const myName = 'Balotte Nathalie';
+let isInWCS = false;
+const schoolName = "Wild Code School";
+
+console.log(typeof myAge, typeof myName, typeof isInWCS, typeof schoolName);
+console.log(myAge, myName, isInWCS, schoolName);
+
+myAge++;
+isInWCS = true;
+console.log(myAge, myName, isInWCS, schoolName);
+
