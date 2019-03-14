3d2fcb1b51eef4abe771333534da265955668576
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-03-14 14:43:55 +0100

    Add JonathanGalvaoDiniz.js

---
+++ JonathanGalvaoDiniz.js
@@ -0,0 +1,16 @@
+const HelloWorld = () => 'Hello World!';
+console.log(HelloWorld());
+
+const sameValue = v => v;
+console.log(sameValue('Hello World!'));
+
+const doubleValue = x => x * 2;
+console.log(doubleValue(10));
+
+const multipleValue = (a, b) => a * b;
+console.log(multipleValue(10, 20));
+
+const addQuote = str => `'${str}'`;
+console.log(addQuote(''));
+
+// By Jonathan Galvao Diniz
